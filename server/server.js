674077da70c27644fa0535a2dbd617f93e8272fa
Rohan Parmar <rohanrparmar987@gmail.com>
2025-06-20 00:53:57 +0530

    Clerk & Inngest Setup

--- server/server.js
+++ server/server.js
@@ -2,6 +2,9 @@ import express from 'express';
 import cors from 'cors'
 import 'dotenv/config';
 import connectDB from './configs/db.js';
+import { clerkMiddleware } from '@clerk/express';
+import { serve } from "inngest/express";
+import { inngest, functions } from "./inngest/index.js"
 
 const app = express();
 const port = 3000;
@@ -12,6 +15,9 @@ await connectDB();
 
 app.use(express.json());
 app.use(cors());
+app.use(clerkMiddleware());
+app.use("/api/inngest", serve({ client: inngest, functions }));
+
 
 app.get('/', (req, res) => {
     res.send('Server is Live!')
