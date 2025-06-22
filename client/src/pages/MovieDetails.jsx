38b55b0e0f960a93363a0ed806d1ac5657a25c9b
Rohan Parmar <rohanrparmar987@gmail.com>
2025-06-22 23:15:02 +0530

    Route and CSS Setup

--- client/src/pages/MovieDetails.jsx
+++ client/src/pages/MovieDetails.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const MovieDetails = () => {
   return (
     <div>
-      
+      <h1>MovieDetails</h1>
     </div>
   )
 }
--- client/src/pages/MyBookings.jsx
+++ client/src/pages/MyBookings.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const MyBooking = () => {
   return (
     <div>
-      
+      <h1>MyBooking</h1>
     </div>
   )
 }
