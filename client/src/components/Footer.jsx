55f06c48042a666598b1e22ef2c67c1bdc30d63f
Rohan Parmar <rohanrparmar987@gmail.com>
2025-06-24 22:03:20 +0530

    Footer Component

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,11 +1,55 @@
-import React from 'react'
+import React from "react";
+import { assets } from "../assets/assets";
+import { Link } from "react-router-dom";
 
 const Footer = () => {
   return (
-    <div>
-      <h1>Footer</h1>
-    </div>
-  )
-}
+    <footer className="px-6 md:px-16 lg:px-36 mt-30 w-full text-gray-300">
+      <div className="flex flex-col md:flex-row justify-between w-full gap-10 border-b border-gray-500 pb-10">
+        <div className="md:max-w-96">
+          <img
+            className="w-36 h-auto"
+            src={assets.logo}
+            alt="logo"
+          />
+          <p className="mt-6 text-sm">
+            Lorem Ipsum has been the industry's standard dummy text ever since
+            the 1500s, when an unknown printer took a galley of type and
+            scrambled it to make a type specimen book.
+          </p>
+        </div>
+        <div className="flex-1 flex items-start md:justify-end gap-20 md:gap-40">
+          <div>
+            <h2 className="font-semibold mb-5">Company</h2>
+            <ul className="text-sm space-y-2">
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/about">About us</Link>
+              </li>
+              <li>
+                <Link to="/contact">Contact us</Link>
+              </li>
+              <li>
+                <Link to="/privacy-policy">Privacy policy</Link>
+              </li>
+            </ul>
+          </div>
+          <div>
+            <h2 className="font-semibold mb-5">Get in touch</h2>
+            <div className="text-sm space-y-2">
+              <p>+1-234-567-890</p>
+              <p>contact@example.com</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <p className="pt-4 text-center text-sm pb-5">
+        Copyright {new Date().getFullYear()} © Rohan Parmar. All Right Reserved.
+      </p>
+    </footer>
+  );
+};
 
-export default Footer
+export default Footer;
