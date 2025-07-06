cd45f9883c0e659284b586bfe6dabb98d7eb57ce
Rohan Parmar <rohanrparmar987@gmail.com>
2025-07-06 11:26:04 +0530

    User Controller and Routes

--- server/controllers/adminController.js
+++ server/controllers/adminController.js
@@ -23,6 +23,7 @@ export const getDashboardData = async (req, res) => {
         }
         res.json({success: true, dashboardData});
     } catch (error) {
+        console.log(error);
         res.json({success: false, message: error.message});
     }
 }
@@ -33,6 +34,7 @@ export const getAllShows = async (req, res) => {
         const shows = await Show.find({showDateTime: {$gte: new Date()}}).populate('movie').sort({showDateTime: 1});
         res.json({success: true, shows});
     } catch (error) {
+        console.log(error);
         res.json({success: false, message: error.message});
     }
 }
@@ -46,6 +48,7 @@ export const getAllBookings = async (req, res) => {
         }).sort({createdAt:-1});
         res.json({success: true, bookings});
     } catch (error) {
+        console.log(error);
         res.json({success: false, message: error.message});
     }
 }
--- server/server.js
+++ server/server.js
@@ -8,6 +8,7 @@ import { inngest, functions } from "./inngest/index.js"
 import showRouter from './routes/showRoutes.js';
 import bookingRouter from './routes/bookingRoutes.js';
 import adminRouter from './routes/adminRoutes.js';
+import userRouter from './routes/userRoutes.js';
 
 const app = express();
 const port = 3000;
@@ -22,8 +23,8 @@ app.use(clerkMiddleware());
 app.use("/api/inngest", serve({ client: inngest, functions }));
 app.use("/api/show", showRouter);
 app.use("/api/booking", bookingRouter);
-app.use("/api/admin", adminRouter)
-
+app.use("/api/admin", adminRouter);
+app.use("/api/user", userRouter);
 
 
 app.get('/', (req, res) => {
