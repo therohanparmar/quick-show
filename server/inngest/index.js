e85d8bfd81c85ca34baaadf28c1ec5bc7dbba37f
Rohan Parmar <rohanrparmar987@gmail.com>
2025-07-20 12:06:07 +0530

    Booking Confirmation Email

--- server/inngest/index.js
+++ server/inngest/index.js
@@ -2,6 +2,7 @@ import { Inngest } from "inngest";
 import User from "../models/User.js"
 import Booking from "../models/Booking.js";
 import Show from "../models/Show.js";
+import sendEmail from "../configs/nodeMailer.js";
 
 // Create a client to send and receive events
 export const inngest = new Inngest({ id: "quick-show" });
@@ -74,5 +75,43 @@ const releaseSeatsAndDeleteBooking = inngest.createFunction(
     }
 )
 
-// Create an empty array where we'll export future Inngest functions
-export const functions = [syncUserCreation, syncUserDeletion, syncUserUpdation, releaseSeatsAndDeleteBooking];
+// Send email when user books a show
+const sendBookingConfirmationEmail = inngest.createFunction(
+    {id: 'send-booking-confirmation-email'},
+    {event: 'app/show.booked'},
+    async ({ event, step}) => {
+        const bookingId = event.data.bookingId;
+        const booking = await Booking.findById(bookingId).populate({
+            path: 'show',
+            populate: {
+                path: 'movie',
+                model: 'Movie'
+            }
+        }).populate('user');
+
+        await sendEmail({
+            to: booking.user.email,
+            subject: `Payment Confirmation: "${booking.show.movie.title}" booked!`,
+            body: `
+                <div style="font-family: Arial, sans-serif; line-height: 1.5;">
+                    <h2>Hi ${booking.user.name},</h2>
+                    <p>Your booking for <strong style="color: #F84565;">"${booking.show.movie.title}"</strong> is confirmed.</p>
+                    <p>
+                        <strdng>Date:</strong> ${new Date(booking.show.showDateTime).toLocaleDateString('en-US', { timeZone:'Asia/Kolkata' })}<br/> |
+                        <strong>Time:</strong> ${new Date (booking.show.showDateTime).toLocaleTimeString('en-US', { timeZone: 'Asia/Kolkata' })}
+                    </p>
+                    <p>Enjoy the show! 🍿</p> |
+                    <p>Thanks for booking with us!<br/>— QuickShow Team</p>
+                <div>
+            `
+        })
+    }
+)
+
+export const functions = [
+    syncUserCreation,
+    syncUserDeletion,
+    syncUserUpdation,
+    releaseSeatsAndDeleteBooking,
+    sendBookingConfirmationEmail,
+];
