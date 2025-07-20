811d6b9df81903a2b192cabe66f85baa327ca10f
Rohan Parmar <rohanrparmar987@gmail.com>
2025-07-20 20:27:09 +0530

    Resolve Time Format Issue

--- client/src/lib/isoTimeFormat.js
+++ client/src/lib/isoTimeFormat.js
@@ -5,7 +5,8 @@ const isoTimeFormat = (dateTime) => {
         {
             hour: "2-digit",
             minute: "2-digit",
-            hour12: true
+            hour12: true,
+            timeZone: 'Asia/Kolkata'
         }
     );
     return localTime;
